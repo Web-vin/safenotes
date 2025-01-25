fa2bef10e0968186d2264c9b86b8d90a65caff0c
vinayak <vinayakgemini07@gmail.com>
2025-01-25 10:53:40 +0530

    Initial commit

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ main().catch(err => console.log(err));
 
 async function main() {
     console.log("connected sucessfuly")
-  await mongoose.connect('mongodb://127.0.0.1:27017/inotes?directConnection=true');
+  await mongoose.connect('mongodb+srv://vinayakjaimini51:Vinayak%4012@cluster0.h6vsu.mongodb.net/inotes');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
